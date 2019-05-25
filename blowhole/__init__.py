b3bced6a6aa653c72ac1671149116a2435f6e6b8
Dan Trickey <dan@trickey.io>
2019-05-25 13:34:47 +0100

    Increment version number

--- blowhole/__init__.py
+++ blowhole/__init__.py
@@ -5,7 +5,7 @@ blowhole is a tool for creating, managing, and using
  docker-based development (or other) environments.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = [
     "__version__",
