bb586a38e4c9fc12790942a6bd74935940e29f46
Kajetan Champlewski <contact@kajetan.ch>
2019-05-25 14:37:39 +0000

    Add method for converting string to ImageName.

--- blowhole/core/image.py
+++ blowhole/core/image.py
@@ -12,3 +12,14 @@ class ImageName:
     def __init__(self, repository: str, tag: Optional[str] = None) -> None:
         self.repository = repository
         self.tag = tag
+
+    @classmethod
+    def from_str(cls, full_name: str) -> 'ImageName':
+        """Takes a full image name string and returns equivalent ImageName."""
+        name = full_name.split(":")
+        if len(name) == 1:
+            return ImageName(name[0])
+        elif len(name) == 2:
+            return ImageName(name[0], name[1])
+        else:
+            raise ValueError("Image name cannot contain multiple tags.")
