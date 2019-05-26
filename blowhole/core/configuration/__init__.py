16086ba426e1eee81af3d84ad8a748ecf6111887
Dan Trickey <dan@trickey.io>
2019-05-26 22:49:24 +0100

    Remove untyped configuration

--- blowhole/core/configuration/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Classes for strictly typed configuration files."""
-
-from .configuration import Configuration
-
-__all__ = [
-    "Configuration",
-]
--- stubs/ruamel/yaml.pyi
+++
@@ -1,7 +0,0 @@
-"""Type stubs for ruamel.yaml."""
-
-from typing import Dict, IO, Union
-
-class YAML:
-
-    def load(self, pointer: IO[str]) -> Dict[str, Union[str, int, float]]: ...
