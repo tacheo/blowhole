25774cb03591e5092a0bb633a5e18354d7b9bd9e
Dan Trickey <dan@trickey.io>
2019-05-27 15:22:32 +0100

    Fix types in configmodel unit tests

--- tests/core/config/test_config.py
+++ tests/core/config/test_config.py
@@ -1,7 +1,7 @@
 """Test the config model."""
 
 import os
-from pathlib import Path
+from pathlib import PurePath
 
 import pydantic
 import pytest
@@ -17,50 +17,44 @@ EMPTY = os.path.join(CURR_DIR, 'files', 'empty.yml')
 def test_initialisation() -> None:
     """Test ConfigModel initialisation with expected params."""
     c = ConfigModel(file=EMPTY)
-    assert isinstance(c.file, Path)
+    assert isinstance(c.file, PurePath)
     assert c.file.exists()
 
 
 def test_initialisation_with_bad_type() -> None:
     """Test ConfigModel initialisation with bad param types."""
-    with pytest.raises(pydantic.ValidationError) as error:
+    with pytest.raises(pydantic.ValidationError):
         ConfigModel(file=1)
-
-    assert len(error.value.errors()) == 1
 
 
 def test_initialisation_with_wrong_params() -> None:
     """Test config model initialisation with the wrong number of params."""
-    with pytest.raises(pydantic.ValidationError) as error:
+    with pytest.raises(pydantic.ValidationError):
         ConfigModel(file=EMPTY, bees=True, number=42)
 
-    assert len(error.value.errors()) == 2
-
-    with pytest.raises(pydantic.ValidationError) as error:
+    with pytest.raises(pydantic.ValidationError):
         ConfigModel()
-
-    assert len(error.value.errors()) == 1
 
 
 def test_initialisation_with_bad_file_path() -> None:
     """Test config model initialisation with a file that is non-existent."""
     # No such file.
-    with pytest.raises(pydantic.ValidationError) as error:
+    with pytest.raises(pydantic.ValidationError):
         ConfigModel(file=BAD)
 
-    assert len(error.value.errors()) == 1
-
     # Not a file.
-    with pytest.raises(pydantic.ValidationError) as error:
+    with pytest.raises(pydantic.ValidationError):
         ConfigModel(file=os.path.join('not', 'a', 'file'))
-
-    assert len(error.value.errors()) == 1
 
 
 def test_validation_of_assignment() -> None:
     """Test that we validate the assignment of attributes."""
     c = ConfigModel(file=EMPTY)
-    c.file = EMPTY
+    # We ignore the next line, as we are deliberately breaking types
+    # to see if it throws an error.
+    c.file = EMPTY  # type: ignore
 
     with pytest.raises(pydantic.ValidationError):
-        c.file = BAD
+        # We ignore the next line, as we are deliberately breaking types
+        # to see if it throws an error.
+        c.file = BAD  # type: ignore
