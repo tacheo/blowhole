0537f970b0ea45bd7b4d0b18104599d06694a370
Kajetan Champlewski <contact@kajetan.ch>
2019-05-28 18:21:37 +0000

    Use ConfigModel for Component.

--- blowhole/core/module.py
+++ blowhole/core/module.py
@@ -4,11 +4,12 @@ from typing import List, Optional, Union
 
 from pydantic.dataclasses import dataclass
 
+from blowhole.core.config import ConfigModel
 from blowhole.core.image import BuildRecipe, ImageName, RunRecipe
 
 
 @dataclass
-class Component:
+class Component(ConfigModel):
     """An executable part of a module."""
 
     recipe: Union[BuildRecipe, RunRecipe]
--- tests/core/module/test_module.py
+++ tests/core/module/test_module.py
@@ -1,7 +1,19 @@
 """Test module classes."""
 
+from os import path
+
+import pytest
+from pydantic import ValidationError
+
 from blowhole.core.image import BuildRecipe, ImageName, RunRecipe
 from blowhole.core.module import Component, Module
+
+CURR_DIR = path.dirname(__file__)
+
+YAML_EMPTY = path.join(CURR_DIR, "files", "empty.yaml")
+
+COMPONENT_VALID = path.join(CURR_DIR, "files", "component_valid.yaml")
+COMPONENT_INVALID = path.join(CURR_DIR, "files", "component_invalid.yaml")
 
 
 def test_component_initialisation() -> None:
@@ -39,6 +51,30 @@ def test_component_should_run() -> None:
     assert c2.should_run(i4)
     assert c2.should_run(i5)
     assert c2.should_run(i6)
+
+
+def test_component_load_valid() -> None:
+    """Test loading a component from a file."""
+    with open(COMPONENT_VALID) as fp:
+        assert Component.load_from_file(fp) == Component(
+            recipe=BuildRecipe(commands=["RUN cd .."]),
+            compatible=[ImageName("example"), ImageName("test", "13.2")],
+            description="A very exciting component.",
+        )
+
+
+def test_component_load_invalid() -> None:
+    """Test loading an invalid component."""
+    with pytest.raises(ValidationError):
+        with open(COMPONENT_INVALID) as fp:
+            Component.load_from_file(fp)
+
+
+def test_component_load_empty() -> None:
+    """Test loading empty yaml as a component."""
+    with pytest.raises(TypeError):
+        with open(YAML_EMPTY) as fp:
+            Component.load_from_file(fp)
 
 
 def test_module_instantiation() -> None:
