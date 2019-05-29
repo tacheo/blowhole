cb5ade50ea5afebedf28b0e07f8811a852cf5e12
Kajetan Champlewski <contact@kajetan.ch>
2019-05-29 17:30:42 +0000

    Allow none argument to should_run.

--- blowhole/core/module.py
+++ blowhole/core/module.py
@@ -17,9 +17,9 @@ class Component(ConfigModel):
     results: Union[None, ImageName] = None
     description: Optional[str] = None
 
-    def should_run(self, source_image: ImageName) -> bool:
+    def should_run(self, source_image: Optional[ImageName]) -> bool:
         """Should this component be executed for a given source image."""
-        if self.compatible is None:
+        if self.compatible is None or source_image is None:
             return True
         else:
             for image in self.compatible:
