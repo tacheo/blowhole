edf6eeee17923fea457948f52b5ac99bc6e76098
Kajetan Champlewski <contact@kajetan.ch>
2019-05-29 17:40:18 +0000

    Add EnvrionmentDefinition.

--- blowhole/core/environment.py
+++ blowhole/core/environment.py
@@ -1,10 +1,12 @@
 """Build containers and images."""
 
-from typing import Optional
+from typing import List, Optional
 
 from pydantic.dataclasses import dataclass
 
+from blowhole.core.config import ConfigModel
 from blowhole.core.image import BuildRecipe, RunRecipe
+from blowhole.core.module import Module
 
 
 @dataclass
@@ -13,4 +15,35 @@ class EnvironmentRecipe:
 
     build: BuildRecipe
     run: RunRecipe
-    name: Optional[str]
+    name: Optional[str] = None
+
+
+@dataclass
+class EnvironmentDefinition(ConfigModel):
+    """A definition of how to construct an envrionment."""
+
+    modules: List[Module]
+    name: Optional[str] = None
+
+    @property
+    def recipe(self) -> EnvironmentRecipe:
+        """Create a buildable Recipe from this definition."""
+        current_image = None
+        build = BuildRecipe()
+        run = RunRecipe()
+
+        for m in self.modules:
+            for c in m.components:
+                if c.should_run(current_image):
+                    if isinstance(c.recipe, BuildRecipe):
+                        build += c.recipe
+                    elif isinstance(c.recipe, RunRecipe):
+                        run += c.recipe
+                    if c.results is not None:
+                        current_image = c.results
+
+        return EnvironmentRecipe(
+            build=build,
+            run=run,
+            name=self.name,
+        )
