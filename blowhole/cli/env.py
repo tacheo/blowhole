08c94a12faeb0a45c929e238875f15d3ca1196d9
Dan Trickey <dan@trickey.io>
2019-05-31 12:42:07 +0100

    Add command to generate a dockerfile from an environment definition

--- blowhole/cli/env.py
+++ blowhole/cli/env.py
@@ -1,6 +1,10 @@
 """CLI interface for Blowhole Environments."""
 
+from typing import TextIO
+
 import click
+
+from blowhole.core.environment import EnvironmentDefinition
 
 
 @click.group("env")
@@ -10,3 +14,14 @@ def env() -> None:
 
     Manage environments.
     """
+
+
+@env.command()
+@click.argument('envdef', type=click.File('rb'), default='blowhole.yml')
+def df(envdef: TextIO) -> None:
+    """Output the generated dockerfile for the given environment definition file."""
+    env = EnvironmentDefinition.load_from_file(envdef)
+
+    recipe = env.recipe
+
+    click.echo(recipe.dockerfile_str)
